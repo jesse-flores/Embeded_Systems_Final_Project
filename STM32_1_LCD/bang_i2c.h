56ee99f9dc9163690760b00f38935a07d5c4361e
Jeffrso <140964848+Jeffrso@users.noreply.github.com>
2026-08-19 16:34:24 -0400

    Create bang_i2c.h

---
+++ STM32_1_LCD/bang_i2c.h
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "ee14lib.h"
+
+#define SDA D0
+#define SCL D1
+#define HIGH 0b01
+#define LOW 0b00
+
+uint8_t bang_i2c_init();
+uint8_t bang_i2c_deinit();
+uint8_t bang_i2c_write_cmd(uint8_t addr, uint8_t *data, uint16_t len);
+uint8_t bang_i2c_write(uint8_t addr, uint8_t reg, uint8_t *data, uint16_t len);
+void i2c_start();
+void i2c_end();
+bool i2c_write_byte(uint8_t byte);
+// uint8_t bang_i2c_read(uint8_t addr, uint8_t *data, uint16_t len);
